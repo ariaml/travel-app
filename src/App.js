c846ba269b1581d54601110b620056479da95e9a
ariaml <airamariedecastroladera@gmail.com>
2023-07-10 16:38:54 +0800

    newest edit

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import { Button, ButtonGroup } from '@chakra-ui/react'
 import { Input } from '@chakra-ui/react'
 import {Icon} from '@chakra-ui/icon'
 import { FaSun, FaMoon, FaPlane, FaBookmark, FaSearch} from 'react-icons/fa';
-import { ImLocation} from 'react-icons/im';
+import { ImLocation, ImOpt} from 'react-icons/im';
 import { ChakraProvider, Box, Text, Link, VStack, Code, Grid, InputGroup, InputRightElement, Heading, Stack, HStack, Spacer, Flex } from '@chakra-ui/react';
-
+import elnidoimage from './components/elnido.jpg'
+import mayonimage from './components/mayon.jpg'
 
 
 function App() {
 
   const { colorMode, toggleColorMode } = useColorMode();
-  const isDark = colorMode === "dark";
+  const isDark = colorMode === 'dark';
 
   return (
     <Stack>
@@ -30,33 +31,49 @@ function App() {
         <Button ml='2' mr='100' colorScheme='teal' variant='solid' >Log in</Button>
       </HStack>
 
-      <Flex p='5' alignItems='center' justifyContent='center'>
-        <InputGroup width='500px'>
-          <Input 
-            variant='outline' 
-            placeholder='Search Tourists spots' _placeholder={{opacity: 1, color: 'gray.500'}}
-            bg='white'
-            borderRadius='full' 
-            boxShadow='dark-lg'/>
-          <InputRightElement>
-            <Icon as={FaSearch} color='gray.500' />
-          </InputRightElement>
-        </InputGroup>
-      </Flex>
+      <Box w='100%' bgGradient='linear(to-r, blue.500, blue.50)'>
+        <Box bgImage={elnidoimage} bgSize='cover' bgPosition='center' bgRepeat='no-repeat' minHeight='100vh' display='flex' flexDirection='column'>
+          <Flex p='5' alignItems='center' justifyContent='center'>
+            <InputGroup width='500px'>
+              <Input 
+                variant='outline' 
+                placeholder='Search Tourists spots' _placeholder={{opacity: 1, color: 'gray.500'}}
+                bg='white'
+                borderRadius='full' 
+                boxShadow='dark-lg'/>
+              <InputRightElement>
+                <Icon as={FaSearch} color='gray.500' />
+              </InputRightElement>
+            </InputGroup>
+          </Flex>
 
-      <VStack alignItems='flex-start' ml='100' mt='250'>
-        <HStack mb='-5'>
-          <Icon as={ImLocation} boxSize={7}/>
-          <Text fontWeight='semibold'>Palawan, Philippines</Text>
-        </HStack>
-        <Text fontSize='80' fontWeight='bold' mb='-50'>EL NIDO</Text>
-        <Text fontSize='80' fontWeight='bold'>ISLAND</Text>
-        <Text>Experience the pinnacle of exceptional service at the world's best island this summer.</Text>
-        <HStack mt='5'>
-          <Button colorScheme='teal' variant='solid' w='150px'>Explore</Button>
-          <IconButton ml='3' colorScheme='teal' isRound='true' icon={<FaBookmark/>}></IconButton>
-        </HStack>
-      </VStack>
+          <VStack alignItems='flex-start' ml='100' mt='180'>
+            <HStack mb='-5'>
+              <Icon color='white' as={ImLocation} boxSize={7}/>
+              <Text color='white' fontWeight='semibold'>Palawan, Philippines</Text>
+            </HStack>
+            <Text color='white' fontSize='80' fontWeight='bold' mb='-50'>EL NIDO</Text>
+            <Text color='white' fontSize='80' fontWeight='bold'>ISLAND</Text>
+            <Text color='white'>Experience the pinnacle of exceptional service at the world's best island this summer.</Text>
+            <HStack mt='5'>
+              <Button colorScheme='teal' variant='solid' w='150px'>Explore</Button>
+              <IconButton ml='3' colorScheme='teal' isRound='true' icon={<FaBookmark/>}></IconButton>
+            </HStack>
+          </VStack>
+
+          <HStack mt='20' w='100%'>
+            <Box as='button' ml='100' borderRadius='md' boxShadow='dark-lg' bg='white' color='teal' px='7' h='200'>Mayon Volcano</Box>
+            <Box as='button' ml='3' borderRadius='md' boxShadow='dark-lg' bg='white' color='teal' px='7' h='200'>Boracay Island</Box>
+            <Box as='button' ml='3' borderRadius='md' boxShadow='dark-lg' bg='white' color='teal' px='7' h='200'>Chocolate Hills</Box>
+            <Box as='button' ml='3' borderRadius='md' boxShadow='dark-lg' bg='white' color='teal' px='7' h='200'>Calle Crisologo</Box>
+            <Box as='button' ml='3' borderRadius='md' boxShadow='dark-lg' bg='white' color='teal' px='7' h='200'>Batanes Island</Box>
+            <Box as='button' ml='3' borderRadius='md' boxShadow='dark-lg' bg='white' color='teal' px='7' h='200'>Kawasan Falls</Box>
+          </HStack>
+
+        </Box>
+      </Box>
+
+      
 
     </Stack>
   );
